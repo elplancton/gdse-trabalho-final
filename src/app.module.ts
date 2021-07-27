336c0e68b4cbaae8dd057ac5c8ba533b121afe84
Rafael <elplancton@gmail.com>
2021-07-27 08:45:58 -0300

    feat(car): Adicionei o recurso Car

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }), CarModule],
   controllers: [AppController],
   providers: [AppService],
 })
